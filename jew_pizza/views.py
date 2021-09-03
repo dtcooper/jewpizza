5ab5f9103e477879f7fd8778928f90414401cd2f
David Cooper <david@dtcooper.com>
2021-09-03 00:27:37 -0400

    Minor tweaks

--- jew_pizza/views.py
+++ jew_pizza/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'home.html', {'title': 'jew.pizza'})
+    return render(request, 'home.html', {'title': 'jew.pizza Demo Page'})
