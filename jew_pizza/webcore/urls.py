c551da11528bece7f6048e6f4b77a61ffb5b4e2f
David Cooper <david@dtcooper.com>
2021-09-06 04:25:16 -0400

    Website!

--- jew_pizza/webcore/urls.py
+++ jew_pizza/webcore/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
+import webcore.views as views
+
 
 app_name = 'webcore'
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='webcore/index.html',
-                                  extra_context={'title': 'jew.pizza Demo Site'}), name='index'),
+    path('', views.index, name='index'),
 ]
--- jew_pizza/webcore/views.py
+++ jew_pizza/webcore/views.py
@@ -1,3 +1,13 @@
+import datetime
+
+from pytz import timezone
+
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+
+    return render(request, 'webcore/index.html', {
+        'title': 'jew.pizza - David Cooper',
+        'eastern_tz_abbrev': timezone('US/Eastern').localize(datetime.datetime.now()).tzname()
+    })
