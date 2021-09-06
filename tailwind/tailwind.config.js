f54baa1dfa80bc3dda042ba513dc435f7fb94d67
David Cooper <david@dtcooper.com>
2021-09-05 22:06:08 -0400

    Commit progress

--- tailwind/tailwind.config.js
+++ tailwind/tailwind.config.js
@@ -43,5 +43,39 @@ module.exports = {
         }
     },
     variants: {},
-    plugins: [],
+    plugins: [
+        require('daisyui'),
+    ],
+    daisyui: {
+        themes: [{
+            'jewpizza': {
+                "fontFamily": "Space Mono,ui-monospace,SFMono-Regular,Menlo,Monaco,Consolas,Liberation Mono,Courier New,monospace",
+                "primary": "#ff7598",
+                "primary-focus": "#ff5784",
+                "primary-content": "#000000",
+                "secondary": "#75d1f0",
+                "secondary-focus": "#5abfdd",
+                "secondary-content": "#000000",
+                "accent": "#c07eec",
+                "accent-focus": "#af59e8",
+                "accent-content": "#000000",
+                "neutral": "#423f00",
+                "neutral-focus": "#090901",
+                "neutral-content": "#ffee00",
+                "base-100": "#ffee00",
+                "base-200": "#dbcd00",
+                "base-300": "#b8ab00",
+                "base-content": "#000000",
+                "info": "#2094f3",
+                "success": "#009485",
+                "warning": "#ff9900",
+                "error": "#ff5724",
+                "--btn-text-case": "none",
+                "--rounded-box": "0",
+                "--rounded-btn": "0",
+                "--rounded-badge": "0",
+                "--tab-radius": "0"
+            },
+        }],
+    },
 }
