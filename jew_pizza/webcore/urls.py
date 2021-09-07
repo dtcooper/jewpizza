99cee3d5e5ba2e5e64d4ca25796b28bafde49f7c
David Cooper <david@dtcooper.com>
2021-09-07 03:37:05 -0400

    Add empty index page

--- jew_pizza/webcore/urls.py
+++ jew_pizza/webcore/urls.py
@@ -7,4 +7,5 @@ import webcore.views as views
 app_name = 'webcore'
 urlpatterns = [
     path('', views.PlaceholderView.as_view(), name='placeholder'),
+    path('index/', views.IndexView.as_view(), name='index'),
 ]
--- jew_pizza/webcore/views.py
+++ jew_pizza/webcore/views.py
@@ -2,6 +2,7 @@ import datetime
 
 from pytz import timezone
 
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
 
 
@@ -14,3 +15,8 @@ class PlaceholderView(TemplateView):
             'eastern_tz_abbrev': timezone('US/Eastern').localize(datetime.datetime.now()).tzname(),
             **super().get_context_data(**kwargs),
         }
+
+class IndexView(LoginRequiredMixin, TemplateView):
+    raise_exception = True  # XXX Delete me when LoginRequiredMixin removed
+    template_name = 'webcore/index.html'
+    extra_context = {'title': 'jew.pizza - David Cooper'}
