3377d017f43988ac2dc30512394193e5474a8f21
David Cooper <david@dtcooper.com>
2021-09-09 03:29:09 -0400

    Add static file cache busting

--- backend/jew_pizza/j2_env.py
+++ backend/jew_pizza/j2_env.py
@@ -1,3 +1,5 @@
+import hashlib
+import os
 import random
 
 from jinja2 import Environment
@@ -5,6 +7,8 @@ from jinja2 import Environment
 from django.conf import settings
 from django.templatetags.static import static as django_static
 from django.urls import reverse
+
+STATIC_FILES_SUM_CACHE = {}
 
 
 def shuffle(items):
@@ -16,18 +20,34 @@ def shuffle(items):
 
 
 def static(path, *args, **kwargs):
+    path_hash = None
+
     if not settings.DEBUG:
-        for ext in ('.js', '.css'):
-            if path.endswith(ext) and not path.endswith(f'.min{ext}'):
-                path = f"{path.removesuffix(ext)}.min{ext}"
-    return django_static(path, *args, **kwargs)
+        for ext in (".js", ".css"):
+            if path.endswith(ext):
+                if not path.endswith(f".min{ext}"):
+                    path = f"{path.removesuffix(ext)}.min{ext}"
+
+                path_hash = STATIC_FILES_SUM_CACHE.get(path)
+                if path_hash is None:
+                    local_path = os.path.join(settings.STATIC_ROOT, path)
+                    if os.path.exists(local_path):
+                        with open(local_path, "rb") as file_to_check:
+                            path_hash = hashlib.md5(file_to_check.read()).hexdigest()
+                    else:
+                        path_hash = False
+                    STATIC_FILES_SUM_CACHE[path] = path_hash
+
+    static_path = django_static(path, *args, **kwargs)
+    if path_hash:
+        static_path = f"{static_path}?v={path_hash}"
+    return static_path
 
 
 def environment(**options):
     env = Environment(**options)
     env.globals.update(
         {
-            "randint": random.randint,
             "settings": settings,
             "shuffle": shuffle,
             "static": static,
