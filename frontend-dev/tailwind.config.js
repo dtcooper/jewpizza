be34efcb68abb3eae98e4913d99dd804227f33f6
David Cooper <david@dtcooper.com>
2021-09-15 14:40:54 -0400

    Make it clear we're using cyberpunk a little more

--- frontend-dev/tailwind.config.js
+++ frontend-dev/tailwind.config.js
@@ -1,5 +1,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 const colors = require('tailwindcss/colors')
+const cyberpunkTheme = require('daisyui/colors/themes')['[data-theme=cyberpunk]']
+
 delete colors.lightBlue
 
 module.exports = {
@@ -49,32 +51,9 @@ module.exports = {
   daisyui: {
     themes: [{
       jewpizza: {
-        fontFamily: 'Space Mono,ui-monospace,SFMono-Regular,Menlo,Monaco,Consolas,Liberation Mono,Courier New,monospace',
-        primary: '#ff7598',
-        'primary-focus': '#ff5784',
-        'primary-content': '#000000',
-        secondary: '#75d1f0',
-        'secondary-focus': '#5abfdd',
-        'secondary-content': '#000000',
-        accent: '#c07eec',
-        'accent-focus': '#af59e8',
-        'accent-content': '#000000',
-        neutral: '#423f00',
-        'neutral-focus': '#090901',
-        'neutral-content': '#ffee00',
-        'base-100': '#ffee00',
-        'base-200': '#dbcd00',
-        'base-300': '#b8ab00',
-        'base-content': '#000000',
-        info: '#2094f3',
-        success: '#009485',
-        warning: '#ff9900',
-        error: '#ff5724',
+        ...cyberpunkTheme,
         '--btn-text-case': 'none',
-        '--rounded-box': '0',
-        '--rounded-btn': '0',
-        '--rounded-badge': '0',
-        '--tab-radius': '0'
+        fontFamily: 'Space Mono,ui-monospace,SFMono-Regular,Menlo,Monaco,Consolas,Liberation Mono,Courier New,monospace'
       }
     }]
   }
