88b32d25aacc1cda1686cb49e9f9ca04fd672ca6
David Cooper <david@dtcooper.com>
2021-10-08 19:13:05 -0400

    Revert "Added git rev to HTML"
    Doesn't work if volume isn't mounted
    
    This reverts commit bfc56d26fa622c61c2f5921b9377aa603a51807a.

--- backend/jew_pizza/utils.py
+++ backend/jew_pizza/utils.py
@@ -1,6 +1,3 @@
-import subprocess
-
-
 def get_client_ip(request):
     x_forwarded_for = request.META.get("HTTP_X_FORWARDED_FOR")
     if x_forwarded_for:
@@ -8,9 +5,3 @@ def get_client_ip(request):
     else:
         ip = request.META.get("REMOTE_ADDR")
     return ip or "[unknown]"
-
-
-try:
-    GIT_REV = subprocess.check_output(['git', 'describe', '--tags', '--always', '--dirty'], text=True).strip()
-except Exception:
-    GIT_REV = 'unknown'
