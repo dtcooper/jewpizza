6e0ffcb5f8bcaa71bc9dfb26b9703b38ba13efd0
David Cooper <david@dtcooper.com>
2021-10-08 18:03:03 -0400

    fe style tweaks

--- backend/shows/urls.py
+++ backend/shows/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
+from . import views
+
+
 app_name = "shows"
 urlpatterns = [
-    path(
-        "shows/",
-        TemplateView.as_view(template_name="webcore/placeholder.html", extra_context={"title": "Shows"}),
-        name="shows",
-    ),
+    path("shows/", views.ShowsMasterListView.as_view(), name="shows-master-list"),
     path(
         "listen/",
         TemplateView.as_view(template_name="webcore/placeholder.html", extra_context={"title": "Listen"}),
--- backend/webcore/constants.py
+++ backend/webcore/constants.py
@@ -3,7 +3,7 @@ CACHE_KEY_PREFIX_STATIC_ASSET_MD5SUM = "static-asset-md5sum::"
 NAVIGATION_LINKS = [
     # name, url_name, icon, is_subnav
     ("Home", "webcore:home", "ic:baseline-home", False),
-    ("Shows", "shows:shows", "mdi:calendar-check-outline", False),
+    ("Shows", "shows:shows-master-list", "mdi:calendar-check-outline", False),
     ("Listen", "shows:listen", "ic:outline-radio", False),
     ("Bio", "webcore:bio", "mdi:text-account", False),
     ("Newsletter", "notifications:newsletter", "mdi:email-newsletter", True),
