9315b3c4b5850d2df344af7941c120635c90fd11
David Cooper <david@dtcooper.com>
2022-01-10 03:10:34 -0500

    Customize bucket

--- backend/webcore/management/commands/cors.py
+++ backend/webcore/management/commands/cors.py
@@ -10,9 +10,15 @@ from django.core.management.base import BaseCommand, CommandError
 class Command(BaseCommand):
     def add_arguments(self, parser):
         group = parser.add_mutually_exclusive_group()
-        group.add_argument("-g", "--get", action="store_true", help="Get CORS for bucket")
-        group.add_argument("-s", "--set", action="store_true", help="Set CORS for bucket")
-        group.add_argument("-a", "--set-all", action="store_true", help="Set for all domains (*)")
+        group.add_argument("-g", "--get", action="store_true", help="get CORS for bucket")
+        group.add_argument("-s", "--set", action="store_true", help="set CORS for bucket")
+        group.add_argument("-a", "--set-all", action="store_true", help="set for all domains (*)")
+        parser.add_argument(
+            "-b",
+            "--bucket",
+            help=f"bucket to set for (default: {settings.AWS_STORAGE_BUCKET_NAME})",
+            default=settings.AWS_STORAGE_BUCKET_NAME,
+        )
         parser.add_argument(
             "domains",
             nargs="*",
@@ -22,6 +28,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         session = boto3.session.Session()
+        bucket = options["bucket"]
         client = session.client(
             "s3",
             region_name=settings.AWS_S3_REGION_NAME,
@@ -48,14 +55,14 @@ class Command(BaseCommand):
                 ]
             }
 
-            client.put_bucket_cors(Bucket=settings.AWS_STORAGE_BUCKET_NAME, CORSConfiguration=cors_configuration)
+            client.put_bucket_cors(Bucket=bucket, CORSConfiguration=cors_configuration)
 
         try:
-            response = client.get_bucket_cors(Bucket=settings.AWS_STORAGE_BUCKET_NAME)
+            response = client.get_bucket_cors(Bucket=bucket)
         except ClientError as e:
             if e.response["Error"]["Code"] == "NoSuchCORSConfiguration":
-                raise CommandError(f"No CORS configuration for {settings.AWS_STORAGE_BUCKET_NAME}")
+                raise CommandError(f'No CORS configuration for bucket "{bucket}"')
             else:
                 raise e
 
-        print(json.dumps(response["CORSRules"], indent=2, sort_keys=True))
+        print(f'CORS rules for bucket "{bucket}": {json.dumps(response["CORSRules"], indent=2, sort_keys=True)}')
