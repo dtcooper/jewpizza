d2f99105fa2887996408a7a9f554d90510cfb0ff
David Cooper <david@dtcooper.com>
2022-02-07 04:45:03 -0500

    nginx container fixes

--- nginx/image/docker-entrypoint.d/25-jinja2-cli-config.sh
+++ nginx/image/docker-entrypoint.d/25-jinja2-cli-config.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
 SCRIPT="$(basename "$0")"
+TEMPLATE_FILE=/etc/nginx/templates/jewpizza.conf.j2
+CONF_FILE=/etc/nginx/conf.d/jewpizza.conf
 
-echo "$SCRIPT: Rendering jinja2 template (jewpizza.conf.j2)"
-jinja2 --format=env /etc/nginx/templates/jewpizza.conf.j2 /.env | tee /etc/nginx/conf.d/jewpizza.conf
+echo "$SCRIPT: Rendering jinja2 template ($TEMPLATE_FILE)"
+jinja2 --format=env "$TEMPLATE_FILE" /.env > "$CONF_FILE"
+echo "$SCRIPT: Rendered config ($CONF_FILE)"
+
+if [ "$DEBUG" -a "$DEBUG" != '0' ]; then
+    echo "------------- $CONF_FILE -------------"
+    cat "$CONF_FILE"
+    echo "------------- $CONF_FILE -------------"
+fi
--- nginx/image/docker-entrypoint.d/40-set-digitalocean-token.sh
+++ nginx/image/docker-entrypoint.d/40-set-digitalocean-token.sh
@@ -14,7 +14,7 @@ elif [ -z "$DEBUG" -o "$DEBUG" = '0' ]; then
     echo "$SCRIPT: DIGITALOCEAN_API_TOKEN needs to be set. See .env file."
     exit 1
 
-elif [ "$USE_LOCAL_CA" -ne '1' ]; then
+elif [ "$USE_LOCAL_CA" != '1' ]; then
     echo "$SCRIPT: set NGINX_DEBUG_MODE_ONLY_USE_LOCAL_CERTIFICATE_AUTHORITY=1 or DIGITALOCEAN_API_TOKEN. See .env file."
     exit 1
 fi
