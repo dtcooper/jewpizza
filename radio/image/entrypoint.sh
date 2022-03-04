3e1305b54db862898915b531ae6f77b1fbc114d0
David Cooper <david@dtcooper.com>
2022-03-04 16:40:04 -0500

    Attempt to fix radio memleak

--- radio/image/entrypoint.sh
+++ radio/image/entrypoint.sh
@@ -9,28 +9,8 @@ else
     exit 1
 fi
 
-if [ "$DEBUG" -a "$DEBUG" != '0' ]; then
-    DEBUG=1
-else
-    DEBUG=
-fi
-
+SCRIPT_URL="http://app:8000/internal/radio/liquidsoap/script/"
 SCRIPT="/var/liquidsoap/script.liq"
-
-run_liquidsoap_loop_debug () {
-    while true; do
-        liquidsoap -f "$SCRIPT"
-    done
-}
-
-run_restart_loop_debug () {
-    while true; do
-        # Auto-reload on changes to backend/radio/jinja2/radio/*.liq (mounted as /watch)
-        inotifywait -qq -e modify -e move -e create -e delete -e attrib /watch/
-        echo "DEBUG: Reloading script based on file modification"
-        wget -qO /dev/null --header="X-Secret-Key: $SECRET_KEY" http://localhost:8000/reload
-    done
-}
 
 wait_for_service() {
     echo "Waiting for ${1}..."
@@ -45,20 +25,11 @@ if [ "$#" != 0 ]; then
     exec "$@"
 else
     wait_for_service app:8000
-    wait_for_service redis:6379
 
-    sed -i "s/^SECRET_KEY.*/SECRET_KEY = '$SECRET_KEY'  # from entrypoint.sh/" "$SCRIPT"
-    sed -i "s/^DEBUG.*/DEBUG = $([ "$DEBUG" ] && echo 'true' || echo 'false')  # from entrypoint.sh/" "$SCRIPT"
-    echo "Replaced SECRET_KEY and DEBUG in $SCRIPT"
-
-    if [ "$DEBUG" -a -d /watch ]; then
-        trap kill_children SIGINT
-        trap kill_children SIGTERM
-        trap kill_children EXIT
-        run_liquidsoap_loop_debug &
-        run_restart_loop_debug &
-        wait
-    else
-        exec liquidsoap -f "$SCRIPT"
+    wget -O "$SCRIPT" --header "X-Secret-Key: $SECRET_KEY" "$SCRIPT_URL"
+    if [ "$DEBUG" -a "$DEBUG" != '0' ]; then
+        pygmentize -l ruby "$SCRIPT" | cat -n
     fi
+
+    exec liquidsoap -f "$SCRIPT"
 fi
