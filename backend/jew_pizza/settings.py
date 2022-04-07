7f3a8a6533db28599f067aa10d6a9583a45c773a
David Cooper <david@dtcooper.com>
2022-04-07 18:27:04 -0400

    Reorganize constance [deploy]

--- backend/jew_pizza/settings.py
+++ backend/jew_pizza/settings.py
@@ -267,7 +267,7 @@ CONSTANCE_CONFIG = {
 
 CONSTANCE_CONFIG_FIELDSETS = OrderedDict(
     (
+        ("Options", ("TWILIO_FROM_NUMBER", "UMAMI_WEBSITE_ID")),
         ("Development Options", ("ENABLE_JEWIPPY", "ENABLE_PLAYER", "ENABLE_TEST_NOTIFICATIONS", "HIDDEN_IMG_MODE")),
-        ("Miscellaneous", ("TWILIO_FROM_NUMBER", "UMAMI_WEBSITE_ID")),
     )
 )
