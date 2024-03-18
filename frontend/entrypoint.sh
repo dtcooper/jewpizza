7dc08fbc00402b4c3ea36541ed70ef2a4294a100
David Cooper <david@dtcooper.com>
2024-03-18 14:50:45 -0400

    Add basic frontend container

---
+++ frontend/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -e
+
+if [ "$#" = 0 ]; then
+    if [ -z "$DEV_MODE" -o "$DEV_MODE" = '0' ]; then
+        echo 'Copying static files to /static/frontend'
+        rsync -a --delete /app/build/client/ /static/frontend
+        SHUTDOWN_TIMEOUT=2 PROTOCOL_HEADER=x-forwarded-proto PORT=8000 exec node build
+    else
+        npm install
+        exec npm run dev -- --port 8000 --host
+    fi
+else
+    echo "Executing: $*"
+    exec "$@"
+fi
--- nginx/image/docker-entrypoint.d/25-jinja2-on-templates.sh
+++ nginx/image/docker-entrypoint.d/25-jinja2-on-templates.sh
@@ -14,8 +14,6 @@ auto_jinja2() {
   local template_dir="${NGINX_JINJA2_TEMPLATE_DIR:-/etc/nginx/templates}"
   local suffix="${NGINX_JINJA2_TEMPLATE_SUFFIX:-.j2}"
   local output_dir="${NGINX_JINJA2_OUTPUT_DIR:-/etc/nginx/conf.d}"
-  local env_file="${NGINX_JINJA2_ENV_FILE:-/.env}"
-  local json_env_file=/tmp/jewpizza-env.json
 
   local template relative_path output_path subdir
   if [ ! -w "$output_dir" ]; then
@@ -23,20 +21,20 @@ auto_jinja2() {
     return 0
   fi
 
-  if [ ! -f "$env_file" ]; then
-    entrypoint_log "$ME: WARNING: env file $env_file doesn't exist"
-    return 0
-  fi
-
-  dotenv -f "$env_file" list --format=json > "$json_env_file"
-
   find "$template_dir" -follow -type f -name "*$suffix" -print | while read -r template; do
     relative_path="${template#"$template_dir/"}"
     output_path="$output_dir/${relative_path%"$suffix"}"
     subdir=$(dirname "$relative_path")
     mkdir -p "$output_dir/$subdir"
     entrypoint_log "$ME: jinja2 template $template rendered as $output_path"
-    jinja2 --format=json "$template" "$json_env_file" > "$output_path"
+    jinja2 --strict "$template" > "$output_path"
+    nginxfmt --indent 2 "$output_path"
+
+    if [ "$DEV_MODE" -a "$DEV_MODE" != '0' ]; then
+        echo "======= rendered config: $output_path ======="
+        cat "$output_path"
+        echo '============================================='
+    fi
   done
 }
 
