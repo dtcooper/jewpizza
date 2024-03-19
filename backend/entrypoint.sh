2d38460c86fd1f61df375a4cfef89b3266acd9ec
David Cooper <david@dtcooper.com>
2024-03-19 15:57:23 -0400

    Minor tweaks to work on Linux

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -5,6 +5,8 @@ set -e
 if [ "$#" = 0 ]; then
     wait-for-it --timeout 0 --service db:5432 --service redis:6379
 
+    ./manage.py migrate
+
     if [ -z "$DEV_MOD" -o "$DEV_MODE" = '0' ]; then
         ./manage.py collectstatic --noinput
     fi
