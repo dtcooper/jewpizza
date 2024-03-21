e892f6c841ac3a2eeb3b1c263986d7701455c19c
David Cooper <david@dtcooper.com>
2024-03-21 12:41:10 -0400

    Fix production build

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,12 +1,12 @@
 <script>
   import "../app.css"
 
-  import { PUBLIC_DOMAIN_NAME as DOMAIN_NAME, PUBLIC_UMAMI_WEBSITE_ID as UMAMI_WEBSITE_ID } from "$env/static/public"
+  import { env } from '$env/dynamic/public';
 </script>
 
 <svelte:head>
-  {#if UMAMI_WEBSITE_ID}
-    <script defer src="https://a.{DOMAIN_NAME}/s.js" data-website-id={UMAMI_WEBSITE_ID}></script>
+  {#if env.PUBLIC_UMAMI_WEBSITE_ID}
+    <script defer src="https://a.{env.PUBLIC_DOMAIN_NAME}/s.js" data-website-id={env.PUBLIC_UMAMI_WEBSITE_ID}></script>
   {/if}
 </svelte:head>
 
